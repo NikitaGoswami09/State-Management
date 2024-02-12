0921f9898e08c3b972b55f7952b692b9b67c7433
Nikita Goswami <140071405+NikitaGoswami09@users.noreply.github.com>
2024-02-12 17:47:13 +0530

    adeded
    added

---
+++ App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import React from 'react';
+import './App.css';
+import { Counter } from './Counter';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
